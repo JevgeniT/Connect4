c31ad9c4c105d45a0c06752cb0d068a3ed0a927a
jevt <jevgeni88@outlook.com>
2020-02-04 13:47:40 +0200

    cleanup

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -1,35 +1,14 @@
-﻿
-
-using System;
-
-namespace Connect4
+﻿namespace Connect4
 {
     class Program
     {
+
+       
         static void Main(string[] args)
         {
-             new Menu().Run();
-
-
-        //      char[,] field =
-        //      {
-        //          {'1','2','3','4','5','6','r'},
-        //          {'2',' ',' ',' ',' ',' ',' '},
-        //          {'3',' ',' ',' ',' ',' ',' '},
-        //          {'4',' ',' ',' ',' ',' ',' '},
-        //          {'5',' ',' ',' ',' ',' ',' '},
-        //          {'c',' ',' ',' ',' ',' ','c'}
-        //      };
-        //      for (int i = 0; i < 6; i++)
-        //      {
-        //          for (int j = 0; j < 7; j++)
-        //          {
-        //              // Console.Write(field[i,j]+" ");
-        //          }
-        //
-        //          // Console.Write(field[0,i]);
-        //          // Console.WriteLine();
-        //      }
+              new Menu().Run();
+              
+              
         }
          
     }
